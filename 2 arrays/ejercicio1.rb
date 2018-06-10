fd6daccbc78d54c2009cae26d6bf0939d284020b
paskuvan <majopaskuvan@gmail.com>
2018-06-09 21:11:05 -0400

    not resolved :sweat:

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -1,8 +1,20 @@
 # Dado el array:
 # 1. Mostrar en pantalla el primer elemento.
-# 2. Mostrar en pantalla el último elemento.
+# 2. Mostrar en pantalla el ultimo elemento.
 # 3. Mostrar en pantalla todos los elementos.
-# 4. Mostrar en pantalla todos los elementos junto con un índice.
-# 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
+# 4. Mostrar en pantalla todos los elementos junto con un indice.
+# 5. Mostrar en pantalla todos los elementos que se encuentren.
 
-arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+arreglo = [1,2,3,9,1,4,5,2,3,6,6],
+
+puts arreglo [0]
+
+print arreglo, "\n"
+
+arreglo.each_with_index do |foo, i|
+  puts "(#{i}) #{foo}"
+end
+
+arreglo.each_with_index do |foo, i|
+  puts foo if (i % 2).zero?
+end
