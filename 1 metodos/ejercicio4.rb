db70610ab0323cac155a58c5bc1da1a52df3a57d
paskuvan <majopaskuvan@gmail.com>
2018-06-09 20:31:16 -0400

    resolved

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,5 +1,13 @@
 # Crear un metodo que muestre en pantalla un saludo, el metodo
 # debe recibir un parametro, si ese parametro es el string "Hola" el
 # metodo debe mostrar en pantalla "Hola Mundo".
+def saludar(saludo)
+  if saludo == 'Hola'
+    puts 'Hola mundo'
+  else
+    puts 'Hola gusano'
+  end
+end
 
-puts "Hola Mundo"
+saludar('Hola')
+saludar('otro')
--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,9 @@
-# Crear un método que reciba como parámetro dos números enteros positivos
-# e imprima los números pares que existen entre esos dos números.
+# Crear un metodo que reciba como parametro dos numeros enteros positivos
+# e imprima los numeros pares que existen entre esos dos numeros.
+
+def check_even? (a,b)
+  (a..b).each { |i| puts i if i.even? }
+  (b..a).each { |i| puts i if i.even? }
+end
+
+check_even?(2, 10)
