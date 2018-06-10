8b1167bd3f7a152b8c86b6cdbedb5eb46c994783
paskuvan <majopaskuvan@gmail.com>
2018-06-09 21:46:09 -0400

    not resolved :sweat:

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -1,7 +1,26 @@
- # Dado el array:
- # 1. Crear un método para eliminar todos los números pares del arreglo.
- # 2. Crear un método para obtener la suma de todos los elementos del arreglo Utilizando .each
- # 3. Crear un método para obtener el promedio de un arreglo.
- # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
-
 a = [1,2,3,9,1,4,5,2,3,6,6]
+def array_even_ele(array)
+  array.each do
+    array.delete_if { |i| (i % 2).zero? } #si elimina el array
+  end
+end
+print array_even_ele a
+puts "\n"
+def array_sum(array)
+  sum = 0
+  array.each { |i| sum += i } #array par
+  puts sum
+end
+array_sum a
+
+def array_mean(array)
+  mean = array.inject { |sum, i| sum + i }.to_f / array.size
+  puts mean
+end
+array_mean a
+
+def array_plus_one(array)
+  new_array = array.map { |i| i + 1 }
+  print new_array, "\n"
+end
+array_plus_one(a)
